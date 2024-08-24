71b55419f5c837dad5a05fc6c34c3d36d46f8996
Aayush <ayushkarkee6@gmail.com>
2024-08-24 17:54:59 +0545

    setting up browserrouter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <div className="bg-blue-800 py-6">
-        <div className="container mx-auto flex justify-between">
+        <div className="container px-4 mx-auto flex justify-between">
             <span className="text-3xl text-white font-bold tracking-tight">
                 <Link to="/">Holidays</Link>
             </span>
             <span className="flex space-x-2">
-                <Link to="/sign-in" className="flex items-center bg-gray-200 text-blue-600 px-3 font-bold hover:bg-gray-400">SignIN</Link>
+                <Link to="/sign-in" className="flex bg-white items-center rounded-lg text-blue-700 px-3 font-bold hover:bg-gray-200 hover:text-blue-700">SignIN</Link>
             </span>
         </div>
     </div>
