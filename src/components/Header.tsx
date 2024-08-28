cb2cde402568c61326e7c9d891b04dc1be93520c
Aayush <ayushkarkee6@gmail.com>
2024-08-28 17:11:24 +0545

    validateToken api to setCookie

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useAppContext } from "../context/AppContext";
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
   return (
     <div className="bg-blue-800 py-6">
       <div className="container mx-auto flex justify-between">
@@ -8,12 +11,20 @@ const Header = () => {
           <Link to="/">EasyStays</Link>
         </span>
         <span className="flex space-x-2">
-          <Link
-            to="/sign-in"
-            className="flex bg-white items-center rounded-lg text-blue-700 px-3 font-bold hover:bg-gray-200 hover:text-blue-700"
-          >
-            SignIN
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to="/my-Bookings">My Bookings</Link>
+              <Link to="/my-Hotels">My Hotels</Link>
+              <button>SignOut</button>
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center rounded-lg text-blue-700 px-3 font-bold hover:bg-gray-200 hover:text-blue-700"
+            >
+              SignIN
+            </Link>
+          )}
         </span>
       </div>
     </div>
