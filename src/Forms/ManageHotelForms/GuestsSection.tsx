68771f69d25b1e3c87e3155124a54c8fe8bca70a
Aayush <ayushkarkee6@gmail.com>
2024-10-23 15:30:48 +0545

    importing react hook form and formdata

--- src/Forms/ManageHotelForms/GuestsSection.tsx
+++ src/Forms/ManageHotelForms/GuestsSection.tsx
@@ -1,4 +1,11 @@
+import { useFormContext } from "react-hook-form";
+import { HotelFormData } from "./ManageHotelForm";
+
 const GuestsSection = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<HotelFormData>();
   return <div>GuestsSection</div>;
 };
 
