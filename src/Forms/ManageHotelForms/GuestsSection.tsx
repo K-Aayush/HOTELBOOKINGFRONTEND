5b1354ff5e915c2488561575ae32140f1d0e613d
Aayush <ayushkarkee6@gmail.com>
2024-10-23 15:34:41 +0545

    adding adult and children input

--- src/Forms/ManageHotelForms/GuestsSection.tsx
+++ src/Forms/ManageHotelForms/GuestsSection.tsx
@@ -6,7 +6,38 @@ const GuestsSection = () => {
     register,
     formState: { errors },
   } = useFormContext<HotelFormData>();
-  return <div>GuestsSection</div>;
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-3">Guests</h2>
+      <div className="">
+        <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+          Adult
+          <input
+            type="number"
+            min={1}
+            className="border border-gray-300 rounded w-full py-1 px-2 font-normal"
+            {...register("adultCount", { required: "This field is required" })}
+          />
+          {errors.adultCount && (
+            <span className="text-red-500">{errors.adultCount.message}</span>
+          )}
+        </label>
+        <label className="text-gray-700 text-sm font-bold max-w-[50%]">
+          Children
+          <input
+            type="number"
+            min={1}
+            className="border border-gray-300 rounded w-full py-1 px-2 font-normal"
+            {...register("childCount", { required: "This field is required" })}
+          />
+          {errors.childCount && (
+            <span className="text-red-500">{errors.childCount.message}</span>
+          )}
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default GuestsSection;
