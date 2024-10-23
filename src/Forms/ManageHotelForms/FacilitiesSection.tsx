14868b7d9b48532833f90ff226b68cc46dddeda0
Aayush <ayushkarkee6@gmail.com>
2024-10-23 15:17:27 +0545

    adding style and rendering the hotelFacilities

--- src/Forms/ManageHotelForms/FacilitiesSection.tsx
+++ src/Forms/ManageHotelForms/FacilitiesSection.tsx
@@ -12,8 +12,12 @@ const FacilitiesSection = () => {
       <h2 className="text-2xl font-bold mb-3">Facilities</h2>
       <div className="grid grid-cols-5 gap-3">
         {hotelFacilities.map((facility) => (
-          <label>
-            <input type="checkbox" value={facility} />
+          <label className="text-sm flex gap-1 text-gray-700">
+            <input 
+            type="checkbox"
+             value={facility} 
+             
+             />
             <span>{facility}</span>
           </label>
         ))}
