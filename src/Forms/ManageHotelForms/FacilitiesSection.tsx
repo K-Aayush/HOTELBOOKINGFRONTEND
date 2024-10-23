059a1bb7e1e01b70220138dbe24cd35467ef62ca
Aayush <ayushkarkee6@gmail.com>
2024-10-23 15:21:44 +0545

    valiating facility section

--- src/Forms/ManageHotelForms/FacilitiesSection.tsx
+++ src/Forms/ManageHotelForms/FacilitiesSection.tsx
@@ -13,11 +13,19 @@ const FacilitiesSection = () => {
       <div className="grid grid-cols-5 gap-3">
         {hotelFacilities.map((facility) => (
           <label className="text-sm flex gap-1 text-gray-700">
-            <input 
-            type="checkbox"
-             value={facility} 
-             
-             />
+            <input
+              type="checkbox"
+              value={facility}
+              {...register("facilities", {
+                validate: (facilities) => {
+                  if (facilities && facilities.length > 0) {
+                    return true;
+                  } else {
+                    return "At least one facility is required";
+                  }
+                },
+              })}
+            />
             <span>{facility}</span>
           </label>
         ))}
