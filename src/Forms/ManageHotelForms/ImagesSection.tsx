2b926b3bb66f0a41028ffdeec7dc25678ef2bd1d
Aayush <ayushkarkee6@gmail.com>
2024-11-05 13:01:55 +0545

    added styles to image input

--- src/Forms/ManageHotelForms/ImagesSection.tsx
+++ src/Forms/ManageHotelForms/ImagesSection.tsx
@@ -11,7 +11,10 @@ const ImagesSection = () => {
       <h2 className="text-2xl font-bold mb-3">Images</h2>
       <div className="border rounded p-4 flex flex-col gap-4">
         <input
+          className="w-full text-gray-700 font-normal"
           type="file"
+          multiple
+          accept="image/*"
           {...register("imageFiles", {
             validate: (imageFiles) => {
               const totalLength = imageFiles.length;
