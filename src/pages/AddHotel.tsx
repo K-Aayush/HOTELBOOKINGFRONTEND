e3d2ecd79609a84a217478be4458ecc9cbb9d93d
Aayush <ayushkarkee6@gmail.com>
2024-11-11 12:17:12 +0545

    showing toast onSuccess and onError

--- src/pages/AddHotel.tsx
+++ src/pages/AddHotel.tsx
@@ -1,6 +1,17 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../Forms/ManageHotelForms/ManageHotelForm";
+import { useAppContext } from "../context/AppContext";
 
 const AddHotel = () => {
+  const { showToast } = useAppContext();
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR" });
+    },
+  });
   return <ManageHotelForm />;
 };
 
