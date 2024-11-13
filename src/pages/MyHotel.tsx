7284cbecb1bcceb0215b46e899aec3f70445a8e2
Aayush <ayushkarkee6@gmail.com>
2024-11-13 16:13:12 +0545

    adding heading and link in myHotel

--- src/pages/MyHotel.tsx
+++ src/pages/MyHotel.tsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const MyHotel = () => {
-  return <div>MyHotel</div>;
+  return (
+    <div className="space-y-5">
+      <span>
+        <h1 className="text-3xl font-bold">My Hotels</h1>
+        <Link
+          to={"/add-hotel"}
+          className="bg-blue-700 text-white p-2 font-bold text-xl rounded-lg hover:bg-blue-600"
+        >
+          Add Hotel
+        </Link>
+      </span>
+    </div>
+  );
 };
 
 export default MyHotel;
